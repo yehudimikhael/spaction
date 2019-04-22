cd4566e8de2426d69eb55dc79d24d50634b6bc5b
Yehudi Brito <yehudi.brito@concrete.com.br>
2019-04-22 15:38:06 -0300

    Refeito Part1 para continuar Part2

--- part2/part2/wsgi.py
+++ part2/part2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for part1 project.
+WSGI config for part2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'part1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'part2.settings')
 
 application = get_wsgi_application()
--- part2/polls/views.py
+++ part2/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, World")
+
+def index(response):
+    return HttpResponse("Hello World")
