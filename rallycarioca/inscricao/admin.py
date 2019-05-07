b5053a5d9f48b3bc75cfabea6c90cf5a614f25c2
Yehudi Brito <yehudi.brito@concrete.com.br>
2019-05-07 13:43:00 -0300

    Change models Tipo Sanguineo in Pessoa

--- rallycarioca/inscricao/admin.py
+++ rallycarioca/inscricao/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Inscricao, Time, Categoria, Etapa, Evento, Pessoa, Pista, Tp_Sangue, Veiculo
+
 # Register your models here.
+
+admin.site.register(Inscricao)
+admin.site.register(Time)
+admin.site.register(Categoria)
+admin.site.register(Etapa)
+admin.site.register(Evento)
+admin.site.register(Pessoa)
+admin.site.register(Pista)
+admin.site.register(Tp_Sangue)
+admin.site.register(Veiculo)
--- rallycarioca/inscricao/models.py
+++ rallycarioca/inscricao/models.py
@@ -5,26 +5,43 @@ from django.db import models
 class Categoria(models.Model):
     tipo = models.CharField(max_length=100)
 
-# class Tipo_Pessoa(models.Model):
-#     tipo = models.CharField(max_length=100)
-
 class Veiculo(models.Model):
     marca = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     ano = models.IntegerField()
+class Tp_Sangue(models.Model):
+    tipo = models.CharField(max_length=100)
 
 class Pessoa(models.Model):
-    nome_piloto = models.CharField(max_length=200)
-    nome_nagevador = models.CharField(max_length=200)
+    nome = models.CharField(max_length=200)
     cnh = models.CharField(max_length=100)
-    rg = models.CharField(max_length=100)
-    #tipo_san = models
-    phone_piloto = models.CharField(max_length=100)
-    phone_navegador = models.CharField(max_length=100)
-    emerg_piloto = models.CharField(max_length=100)
-    emerg_navegador = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    emerg = models.CharField(max_length=100)
     pl_saude = models.CharField(max_length=100)
-    pl_saude = models.CharField(max_length=100)
-    al_db_piloto = models.CharField(max_length=200)
-    al_db_navegador = models.CharField(max_length=200) ###Variável para o usuário escrever se tem alergia ou Diabetes 
+    al_db = models.CharField(max_length=200) ###Variável para o usuário escrever se tem alergia ou Diabetes 
+    tipo_sang = models.ForeignKey(Tp_Sangue, models.PROTECT, default='tipo_sang')
     #tipo_pessoa = models.ForeignKey(Tipo_Pessoa)
+
+class Time(models.Model):
+    piloto = models.ForeignKey(Pessoa, related_name='piloto', on_delete=models.CASCADE)
+    navegador = models.ForeignKey(Pessoa, related_name='navegador', on_delete=models.CASCADE)
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+
+class Pista(models.Model):
+    nome = models.CharField(max_length=200)
+    local = models.CharField(max_length=200)
+
+class Etapa(models.Model):
+    pista = models.ForeignKey(Pista, on_delete=False)
+    date_corrida = models.DateField(auto_now=False, auto_now_add=False)
+
+class Evento(models.Model):
+    nome = models.CharField(max_length=200)
+    dateInicioInscricao  = models.DateField(auto_now=False, auto_now_add=False)
+    dateFimInscricao = models.DateField(auto_now=False, auto_now_add=False)
+    etapa = models.ManyToManyField(Etapa)
+class Inscricao(models.Model):
+    evento = models.ForeignKey(Evento, models.PROTECT) 
+    time = models.ForeignKey(Time, models.PROTECT)
+    colocao = models.IntegerField()    
+
