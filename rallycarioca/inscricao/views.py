d45d165a77c43f04c7866cdcd63cac20e0fec6e4
Yehudi Brito <yehudi.brito@concrete.com.br>
2019-05-10 14:35:37 -0300

    Inclusao o style.css e configuranção na view

--- rallycarioca/inscricao/views.py
+++ rallycarioca/inscricao/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader, Template, Context
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello World")
+
+def inscricao(request):
+    cadastro = 
