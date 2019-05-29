3db952902ce0e8d0234c484b4874def8c514ac88
Yehudi Brito <yehudi.brito@concrete.com.br>
2019-05-29 17:32:35 -0300

    Correcao model e primeiros cadastros

--- rallycarioca/inscricao/models.py
+++ rallycarioca/inscricao/models.py
@@ -4,13 +4,20 @@ from django.db import models
 
 class Categoria(models.Model):
     tipo = models.CharField(max_length=100)
+    def __str__(self):
+        return self.tipo
 
 class Veiculo(models.Model):
     marca = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     ano = models.IntegerField()
+    def __str__(self):
+        return self.modelo
+
 class Tp_Sangue(models.Model):
     tipo = models.CharField(max_length=100)
+    def __str__(self):
+        return self.tipo
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=200)
@@ -21,27 +28,40 @@ class Pessoa(models.Model):
     al_db = models.CharField(max_length=200) ###Variável para o usuário escrever se tem alergia ou Diabetes 
     tipo_sang = models.ForeignKey(Tp_Sangue, models.PROTECT, default='tipo_sang')
     #tipo_pessoa = models.ForeignKey(Tipo_Pessoa)
+    def __str__(self):
+        return self.nome
 
 class Time(models.Model):
+    name = models.CharField(max_length=100)
     piloto = models.ForeignKey(Pessoa, related_name='piloto', on_delete=models.CASCADE)
     navegador = models.ForeignKey(Pessoa, related_name='navegador', on_delete=models.CASCADE)
     veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name 
 
 class Pista(models.Model):
     nome = models.CharField(max_length=200)
     local = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nome
 
 class Etapa(models.Model):
     pista = models.ForeignKey(Pista, on_delete=False)
     date_corrida = models.DateField(auto_now=False, auto_now_add=False)
-
+    def __str__(self):
+        return self.pista.nome 
+        
 class Evento(models.Model):
     nome = models.CharField(max_length=200)
     dateInicioInscricao  = models.DateField(auto_now=False, auto_now_add=False)
     dateFimInscricao = models.DateField(auto_now=False, auto_now_add=False)
     etapa = models.ManyToManyField(Etapa)
+    def __str__(self):
+        return self.nome
 class Inscricao(models.Model):
     evento = models.ForeignKey(Evento, models.PROTECT) 
     time = models.ForeignKey(Time, models.PROTECT)
-    colocao = models.IntegerField()    
+    colocao = models.IntegerField()
+    def __str__(self):
+        return self.evento.nome   
 
